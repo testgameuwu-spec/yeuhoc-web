27f9311a531e36f35cc4add2ec9e5d1ad92efd7c
zackpdm <pduyminh0903@gmail.com>
2026-05-13 20:57:29 +0700

    sửa yeuhoc vercel thành .site

--- check_env.js
+++ check_env.js
@@ -1,9 +1,9 @@
-fetch('https://yeuhoc-web.vercel.app/profile/')
+fetch('https://yeuhoc.site/profile/')
   .then(r=>r.text())
   .then(html=>{ 
     const jsChunks = html.match(/\/_next\/static\/chunks\/[^\"]+\.js/g); 
     if(jsChunks) { 
-      Promise.all(jsChunks.map(chunk=>fetch('https://yeuhoc-web.vercel.app'+chunk).then(r=>r.text())))
+      Promise.all(jsChunks.map(chunk=>fetch('https://yeuhoc.site'+chunk).then(r=>r.text())))
       .then(texts => { 
         console.log('Found Local DB URL (ahcgigcacmaerammxzqe):', texts.some(t => t.includes('ahcgigcacmaerammxzqe'))); 
       }) 
